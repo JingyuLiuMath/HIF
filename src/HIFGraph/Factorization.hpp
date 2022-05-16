3db0c956af7d275d9401465c94039e8733e1a794
Jingyu <381258337@qq.com>
2022-05-16 16:23:03 +0800

    update Factor

---
+++ src/HIFGraph/Factorization.hpp
@@ -0,0 +1,25 @@
+#include "HIF.hpp"
+
+namespace HIF {
+
+// Factorization
+template <typename Scalar>
+void HIFGraph<Scalar>::Factorization(double tol)
+{
+	for (int tmplevel = numlevels_; tmplevel >= 1; tmplevel--)
+	{
+		// Sparse elimination.
+		RecursiveSparseElim(tmplevel);
+
+		// Skeletonization.
+		RecursiveSkel(tmplevel, tol);
+
+		// Merge.
+		RecursiveMerge(tmplevel - 1);
+	}
+
+	// Root factorization.
+	RootFactorization();
+}
+
+} // namespace HIF
