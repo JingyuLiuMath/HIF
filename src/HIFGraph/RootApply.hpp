44d2e33307a44bc3d23bc56f1f843489d7e5a4b6
Jingyu <381258337@qq.com>
2022-05-17 22:27:06 +0800

    update RootApply

---
+++ src/HIFGraph/RootApply.hpp
@@ -0,0 +1,18 @@
+#include "HIF.hpp"
+
+namespace HIF {
+
+// Apply on the root.
+template <typename Scalar>
+void HIFGraph<Scalar>::RootApply()
+{
+    // xI = LI^ {-1} * xI.
+    Trmm(ELLR::LEFT, LOWER, NORMAL, UNIT, Scalar(1), AIIinv_, xI_);
+    // xI = DI^ { -1 } *xI.
+    MatrixS D = GetDiagonal(AIIinv_);
+    DiagonalScale(ELLR::LEFT, NORMAL, D, xI_);
+    // xI = LI^ { -T } *xI.
+    Trmm(ELLR::LEFT, LOWER, TRANSPOSE, UNIT, Scalar(1), AIIinv_, xI_);
+}
+
+} // namespace HIF.
