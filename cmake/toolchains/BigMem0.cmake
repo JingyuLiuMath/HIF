a33a7cf3a95832da1a77fe74af945b51d148f37e
Jingyu <381258337@qq.com>
2022-05-31 21:07:24 +0800

    update toolchains

--- cmake/toolchains/BigMem0.cmake
+++ cmake/toolchains/BigMem0.cmake
@@ -5,7 +5,7 @@ set(CMAKE_CXX_COMPILER     ${COMPILER_DIR}/g++)
 set(CMAKE_Fortran_COMPILER ${COMPILER_DIR}/gfortran)
 
 # The MPI wrappers for the C and C++ compilers
-set(MPI_COMPILER_DIR /opt/mpich/4.0/bin)
+set(MPI_COMPILER_DIR /opt/MPICH/4.0.2/bin)
 set(MPI_C_COMPILER       ${MPI_COMPILER_DIR}/mpicc)
 set(MPI_CXX_COMPILER     ${MPI_COMPILER_DIR}/mpicxx)
 set(MPI_Fortran_COMPILER ${MPI_COMPILER_DIR}/mpif90)
