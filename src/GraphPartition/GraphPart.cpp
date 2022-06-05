498b73136911d01860f6fb2edd88c91225d62400
Jingyu <381258337@qq.com>
2022-06-05 14:08:04 +0800

    update index

--- src/GraphPartition/GraphPart.cpp
+++ src/GraphPartition/GraphPart.cpp
@@ -174,7 +174,7 @@ void MetisSepPart(SparseMatrix<Scalar>& A,
     idx_t* where;
 
     // set up the run time parameters
-    // ctrl = SetupCtrl(METIS_OP_OMETIS, options, 1, 3, NULL, NULL);
+    ctrl = SetupCtrl(METIS_OP_OMETIS, options, 1, 3, NULL, NULL);
 
     // prune the dense columns
     if (ctrl->pfactor > 0.0)
