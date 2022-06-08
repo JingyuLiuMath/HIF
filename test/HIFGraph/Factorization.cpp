bb7815eb3866f5ac6df1574b311d05fb38454ba2
Jingyu <381258337@qq.com>
2022-06-08 10:57:27 +0800

    debug sparsemtx

--- test/HIFGraph/Factorization.cpp
+++ test/HIFGraph/Factorization.cpp
@@ -65,7 +65,7 @@ int main(int argc, char* argv[])
 		std::cout << A.NumEntries() << std::endl;
 		std::cout << A.Get(6, 6) << std::endl;
 		vector<int> index;
-		for (int i = 0; i < 3; i += 2)
+		for (int i = 1; i < 5; i += 2)
 		{
 			index.push_back(i);
 		}
