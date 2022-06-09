aaa11dcc2b5477dff8a4037a8ae29c2081d90db7
Jingyu <381258337@qq.com>
2022-06-09 10:22:14 +0800

    debug filltree

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -105,6 +105,8 @@ void FullMat(const SparseMatrix<Scalar>& sparseA, Matrix<Scalar>& A)
         for (int j = 0; j < A.Width(); j++)
         {
             std::cout << " i " << i << " j " << j << std::endl;
+            std::cout << sparseA.Offset(i, j) << std::endl;
+            std::cout << sparseA.Size() << std::endl;
             std::cout << "sparseA.Get(i, j)" << sparseA.Get(i, j) << std::endl;
             A.Set(i, j, sparseA.Get(i, j));
         }
