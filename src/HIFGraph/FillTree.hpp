a4feef31ac60c92ed64963824103b0e062eca7ce
Jingyu <381258337@qq.com>
2022-06-09 15:38:47 +0800

    debug filltree

--- src/HIFGraph/FillTree.hpp
+++ src/HIFGraph/FillTree.hpp
@@ -12,22 +12,7 @@ void HIFGraph<Scalar>::FillTree(const SparseMatrix<Scalar>& A)
 		std::cout << " level " << level_ << std::endl;
 		std::cout << " seqnum " << seqnum_ << std::endl;
 
-		for (int i = 0; i < sep_.size(); i++)
-		{
-			std::cout << " sep_[i] " << sep_[i] << std::endl;
-		}
-		for (int i = 0; i < nb_.size(); i++)
-		{
-			std::cout << " nb_[i] " << nb_[i] << std::endl;
-		}
-
 		Diff_Sort(vtx_, sep_, intr_);
-
-		for (int i = 0; i < intr_.size(); i++)
-		{
-			std::cout << " intr_[i] " << intr_[i] << std::endl;
-		}
-		
 
 		SparseMatrixS Aneed;
 		
