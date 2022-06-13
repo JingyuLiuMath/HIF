627881457902dbd5af9e3ac3b7b3382b68cb017f
Jingyu <381258337@qq.com>
2022-06-13 15:44:28 +0800

    update skel

--- include/HIF/hifgraph.hpp
+++ include/HIF/hifgraph.hpp
@@ -56,7 +56,7 @@ public:
     ~HIFGraph();
 
     // Factorization.
-    void Factorization(int HIFbutton, double tol);
+    void Factorization(int HIFbutton = 1, double tol = 1e-3);
 
     // Application.
     void Apply(Matrix<Scalar>& b);
