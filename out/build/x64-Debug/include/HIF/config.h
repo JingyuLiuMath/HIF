f5b475a23791862abc8db480cbde77894f77938a
Jingyu <381258337@qq.com>
2022-06-15 20:26:59 +0800

    add config.h

---
+++ out/build/x64-Debug/include/HIF/config.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef HIF_CONFIG_H
+#define HIF_CONFIG_H 1
+
+#define HIF_VERSION_MAJOR 1
+#define HIF_VERSION_MINOR 0
+/* #undef RELEASE */
+#define TIMER
+#define HIF_INFO
+/* #undef SBATCH */
+
+#define RESTRICT 
+
+#endif /* ifndef HIF_CONFIG_H */
