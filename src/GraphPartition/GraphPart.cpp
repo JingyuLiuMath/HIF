800cc1d7578c2078c41560acfbefc8fd59103931
Jingyu <381258337@qq.com>
2022-06-23 09:41:09 +0800

    remove sort

--- src/GraphPartition/GraphPart.cpp
+++ src/GraphPartition/GraphPart.cpp
@@ -99,10 +99,6 @@ void MetisPart(const SparseMatrix<Scalar>& A,
 
     vector<int> lidx, ridx, sepidx;
     MetisSepPart(A1, lidx, ridx, sepidx);  // lidx, ridx, sepidx are sorted.
-       
-    sort(lidx.begin(), lidx.end());
-    sort(ridx.begin(), ridx.end());
-    sort(sepidx.begin(), sepidx.end());
 
     p1.resize(lidx.size());
     p2.resize(ridx.size());
