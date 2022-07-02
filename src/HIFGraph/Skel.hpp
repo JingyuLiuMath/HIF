f74b6250e0042ed3bb7928fd9767d8b977676550
Jingyu <381258337@qq.com>
2022-07-02 22:36:51 +0800

    a little try

--- src/HIFGraph/Skel.hpp
+++ src/HIFGraph/Skel.hpp
@@ -207,10 +207,10 @@ void HIFGraph<Scalar>::Skel()
 		{
 			myindex_p12[i] = myindex_sep1[p12[i]];
 			nodekindex_p12[i] = nodekindex_sep1[p12[i]];
-			re_.push_back(sep1[p12[i]]);
-			(nodek->nbre_).push_back(sep1[p12[i]]);
-			/*AddElement_Sort(re_, sep1[p12[i]]);
-			AddElement_Sort(nodek->nbre_, sep1[p12[i]]);*/
+			/*re_.push_back(sep1[p12[i]]);
+			(nodek->nbre_).push_back(sep1[p12[i]]);*/
+			AddElement_Sort(re_, sep1[p12[i]]);
+			AddElement_Sort(nodek->nbre_, sep1[p12[i]]);
 		}
 
 		TIMER_HIF(TimerStart(TIMER_EL))
@@ -246,10 +246,10 @@ void HIFGraph<Scalar>::Skel()
 		{
 			myindex_p22[i] = myindex_sep2[p22[i]];
 			nodekindex_p22[i] = nodekindex_sep2[p22[i]];
-			nbre_.push_back(sep2[p22[i]]);
-			(nodek->re_).push_back(sep2[p22[i]]);
-			/*AddElement_Sort(nbre_, sep2[p22[i]]);
-			AddElement_Sort(nodek->re_, sep2[p22[i]]);*/
+			/*nbre_.push_back(sep2[p22[i]]);
+			(nodek->re_).push_back(sep2[p22[i]]);*/
+			AddElement_Sort(nbre_, sep2[p22[i]]);
+			AddElement_Sort(nodek->re_, sep2[p22[i]]);
 		}
 
 		nbinfo_[k].skip = 0;
@@ -439,9 +439,9 @@ void HIFGraph<Scalar>::Skel()
 	}
 
 	// If using AddElement_Sort, no need to sort re and nbre. But no improvement.
-	sort(re_.begin(), re_.end());
+	// sort(re_.begin(), re_.end());
 	Diff_Sort(sep_, re_, sk_);
-	sort(nbre_.begin(), nbre_.end());
+	// sort(nbre_.begin(), nbre_.end());
 	Diff_Sort(nb_, nbre_, nbsk_);
 }
 
