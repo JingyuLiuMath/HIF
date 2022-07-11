978d153f6e3ed85e6db61b1f37f6833ec5f8f2d0
Jingyu <381258337@qq.com>
2022-07-11 23:18:03 +0800

    update filltree

--- src/HIFGraph/FillTree.hpp
+++ src/HIFGraph/FillTree.hpp
@@ -12,47 +12,47 @@ void HIFGraph<Scalar>::FillTree(const SparseMatrixS& A)
 	if (endflag_ == 1)
 	{	
 		Diff_Sort(vtx_, sep_, intr_);
-		// SparseMatrixS Aneed;
+		SparseMatrixS Aneed;
 		
-		/*El::Zeros(AII_, intr_.size(), intr_.size());
+		El::Zeros(AII_, intr_.size(), intr_.size());
 		Aneed = A(intr_, intr_);
 		if ((Aneed.Height() > 0) && (Aneed.Width()> 0))
 		{
 			Aneed.QueueUpdate(Aneed.Height() - 1, Aneed.Width() - 1, Scalar(0));
 			Aneed.ProcessQueues();
 		}
-		FullMat(Aneed, AII_);*/
-		FullMat(A, intr_, intr_, AII_);
+		FullMat(Aneed, AII_);
+		// FullMat(A, intr_, intr_, AII_);
 
-		/*El::Zeros(ASI_, sep_.size(), intr_.size());
+		El::Zeros(ASI_, sep_.size(), intr_.size());
 		Aneed = A(sep_, intr_);
 		if ((Aneed.Height() > 0) && (Aneed.Width() > 0))
 		{
 			Aneed.QueueUpdate(Aneed.Height() - 1, Aneed.Width() - 1, Scalar(0));
 			Aneed.ProcessQueues();
 		}
-		FullMat(Aneed, ASI_);*/
-		FullMat(A, sep_, intr_, ASI_);
+		FullMat(Aneed, ASI_);
+		// FullMat(A, sep_, intr_, ASI_);
 
-		/*El::Zeros(ASS_, sep_.size(), sep_.size());
+		El::Zeros(ASS_, sep_.size(), sep_.size());
 		Aneed = A(sep_, sep_);
 		if ((Aneed.Height() > 0) && (Aneed.Width() > 0))
 		{
 			Aneed.QueueUpdate(Aneed.Height() - 1, Aneed.Width() - 1, Scalar(0));
 			Aneed.ProcessQueues();
 		}
-		FullMat(Aneed, ASS_);*/
-		FullMat(A, sep_, sep_, ASS_);
+		FullMat(Aneed, ASS_);
+		// FullMat(A, sep_, sep_, ASS_);
 
-		/*El::Zeros(ANS_, nb_.size(), sep_.size());
+		El::Zeros(ANS_, nb_.size(), sep_.size());
 		Aneed = A(nb_, sep_);
 		if ((Aneed.Height() > 0) && (Aneed.Width() > 0))
 		{
 			Aneed.QueueUpdate(Aneed.Height() - 1, Aneed.Width() - 1, Scalar(0));
 			Aneed.ProcessQueues();
 		}
-		FullMat(Aneed, ANS_);*/
-		FullMat(A, nb_, sep_, ANS_);
+		FullMat(Aneed, ANS_);
+		// FullMat(A, nb_, sep_, ANS_);
 
 		SetSeparatorType();
 	}
