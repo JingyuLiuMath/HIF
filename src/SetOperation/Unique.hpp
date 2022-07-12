c32628937bdf8f68860f7958e13668f179e82469
Jingyu <381258337@qq.com>
2022-07-12 11:11:10 +0800

    update unique and buildtree

--- src/SetOperation/Unique.hpp
+++ src/SetOperation/Unique.hpp
@@ -9,9 +9,20 @@ void Unique_Sort(vector<Scalar>& vec)
 	DEBUG_HIF(CallStackEntry cse("Unique_Sort"))
 
 	TIMER_HIF(TimerStart(TIMER_SETOP))
-	vector<Scalar>::iterator it;
-	it = std::unique(vec.begin(), vec.end());
-	vec.resize(std::distance(vec.begin(), it));
+	vector<Scalar> vectmp;
+	if (vec.size() == 0)
+	{
+		return;
+	}
+	vectmp.push_back(vec[0]);
+	for (int i = 1; i < vec.size(); i++)
+	{
+		if (vectmp.back() != vec[i])
+		{
+			vectmp.push_back(vec[i]);
+		}
+	}
+	vec.assign(vectmp.begin(), vectmp.end());
 	TIMER_HIF(TimerStop(TIMER_SETOP))
 }
 
