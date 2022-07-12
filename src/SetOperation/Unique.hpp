4dfe625f817527a15362f0dc44ee90bdea7db3c1
Jingyu <381258337@qq.com>
2022-07-12 10:56:16 +0800

    update unique and buildtree

---
+++ src/SetOperation/Unique.hpp
@@ -0,0 +1,18 @@
+#include "HIF.hpp"
+
+namespace HIF {
+
+// vec = unique(vec) where vec is a sorted vec.
+template <typename Scalar>
+void Unique_Sort(vector<Scalar>& vec)
+{
+	DEBUG_HIF(CallStackEntry cse("Unique_Sort"))
+
+	TIMER_HIF(TimerStart(TIMER_SETOP))
+	vector<int>::iterator it;
+	it = std::unique(vec.begin(), vec.end());
+	vec.resize(std::distance(vec.begin(), it));
+	TIMER_HIF(TimerStop(TIMER_SETOP))
+}
+
+} // namespace HIF.
