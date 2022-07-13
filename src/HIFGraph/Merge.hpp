065d247be7b8efd005891801bc47f265a123b6e5
Jingyu <381258337@qq.com>
2022-07-13 19:28:35 +0800

    a try

--- src/HIFGraph/Merge.hpp
+++ src/HIFGraph/Merge.hpp
@@ -44,18 +44,20 @@ void HIFGraph<Scalar>::Merge()
 	// sep : sep \cup children's sk.
 	// nb: nb \cup children's nbsk.
 	vector<int> tmpnbsk;
+	intr_.reserve(children_[0]->sk.size() + children_[1]->sk.size());
+	tmpnbsk.reserve(children_[0]->nbsk.size() + children_[1]->nbsk.size());
 	for (int iter = 0; iter < 2; iter++)
 	{
-		/*intr_.insert(intr_.end(), (children_[iter]->sk_).begin(), (children_[iter]->sk_).end());
-		tmpnbsk.insert(tmpnbsk.end(), (children_[iter]->nbsk_).begin(), (children_[iter]->nbsk_).end());*/
-		for (int i = 0; i < children_[iter]->sk_.size(); i++)
+		intr_.insert(intr_.end(), (children_[iter]->sk_).begin(), (children_[iter]->sk_).end());
+		tmpnbsk.insert(tmpnbsk.end(), (children_[iter]->nbsk_).begin(), (children_[iter]->nbsk_).end());
+		/*for (int i = 0; i < children_[iter]->sk_.size(); i++)
 		{
 			intr_.push_back(children_[iter]->sk_[i]);
 		}
 		for (int i = 0; i < children_[iter]->nbsk_.size(); i++)
 		{
 			tmpnbsk.push_back(children_[iter]->nbsk_[i]);
-		}
+		}*/
 	}
 	std::sort(intr_.begin(), intr_.end());
 	std::sort(tmpnbsk.begin(), tmpnbsk.end());
