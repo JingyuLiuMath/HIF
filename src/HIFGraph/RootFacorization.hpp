9ed64097d4bcb0b3a2609c077b051347d0fb4684
Jingyu <381258337@qq.com>
2022-07-14 18:35:35 +0800

    add log

--- src/HIFGraph/RootFacorization.hpp
+++ src/HIFGraph/RootFacorization.hpp
@@ -7,7 +7,12 @@ void HIFGraph<Scalar>::RootFactorization()
 {
 	DEBUG_HIF(CallStackEntry cse("HIFGraph:RootFactorization"))
 
+	TIMER_HIF(TimerStart(TIMER_EL))
+	TIMER_HIF(TimerStart(TIMER_EL_LDLSOLVE))
 	LDLSolve(AII_, AIIinv_);
+	TIMER_HIF(TimerStop(TIMER_EL_LDLSOLVE))
+	TIMER_HIF(TimerStop(TIMER_EL))
+	
 	// AII = 0.
 	AII_.Empty();
 }
