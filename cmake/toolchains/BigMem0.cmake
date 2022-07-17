9f79c828f382254259f7ae3b17606bf6147c06d0
Jingyu <381258337@qq.com>
2022-07-17 15:08:07 +0800

    bigmem0cmake

--- cmake/toolchains/BigMem0.cmake
+++ cmake/toolchains/BigMem0.cmake
@@ -5,10 +5,10 @@ set(CMAKE_CXX_COMPILER     ${COMPILER_DIR}/g++)
 set(CMAKE_Fortran_COMPILER ${COMPILER_DIR}/gfortran)
 
 # The MPI wrappers for the C and C++ compilers
-set(MPI_COMPILER_DIR /opt/MPICH/4.0.2/bin)
-set(MPI_C_COMPILER       ${MPI_COMPILER_DIR}/mpicc)
-set(MPI_CXX_COMPILER     ${MPI_COMPILER_DIR}/mpicxx)
-set(MPI_Fortran_COMPILER ${MPI_COMPILER_DIR}/mpif90)
+#set(MPI_COMPILER_DIR /opt/MPICH/4.0.2/bin)
+#set(MPI_C_COMPILER       ${MPI_COMPILER_DIR}/mpicc)
+#set(MPI_CXX_COMPILER     ${MPI_COMPILER_DIR}/mpicxx)
+#set(MPI_Fortran_COMPILER ${MPI_COMPILER_DIR}/mpif90)
 
 set(EL_INCLUDE_DIR /home/jyliu/packages/elemental/master/include)
 set(EL_LIB_DIR /home/jyliu/packages/elemental/master/lib64)
