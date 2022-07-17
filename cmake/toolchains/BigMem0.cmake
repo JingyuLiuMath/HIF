846a76f49d9cb59aca88296123456b2145e6b9b3
Jingyu <381258337@qq.com>
2022-07-17 15:11:59 +0800

    correct version

--- cmake/toolchains/BigMem0.cmake
+++ cmake/toolchains/BigMem0.cmake
@@ -4,15 +4,11 @@ set(CMAKE_C_COMPILER       ${COMPILER_DIR}/gcc)
 set(CMAKE_CXX_COMPILER     ${COMPILER_DIR}/g++)
 set(CMAKE_Fortran_COMPILER ${COMPILER_DIR}/gfortran)
 
-# The MPI wrappers for the C and C++ compilers
-#set(MPI_COMPILER_DIR /opt/MPICH/4.0.2/bin)
-#set(MPI_C_COMPILER       ${MPI_COMPILER_DIR}/mpicc)
-#set(MPI_CXX_COMPILER     ${MPI_COMPILER_DIR}/mpicxx)
-#set(MPI_Fortran_COMPILER ${MPI_COMPILER_DIR}/mpif90)
-
+# Elemental
 set(EL_INCLUDE_DIR /home/jyliu/packages/elemental/master/include)
 set(EL_LIB_DIR /home/jyliu/packages/elemental/master/lib64)
 
+# Metis
 set(METIS_INCLUDE_DIR /home/jyliu/metis-5.1.0/include)
 set(METIS_LIBMETIS_DIR /home/jyliu/metis-5.1.0/libmetis)
 set(METIS_GKLIB_DIR /home/jyliu/metis-5.1.0/GKlib)
