60985383299b95d3a80b690c4b5f9faf01d15ef6
Jingyu <381258337@qq.com>
2022-07-21 16:42:49 +0800

    update

--- test/HIFSolve/HIFSolve.cpp
+++ test/HIFSolve/HIFSolve.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
 
 		for (int i = 0; i < b.size(); i++)
 		{
-			cout << b[i] << endl;
+			std::cout << b[i] << std::endl;
 		}
 
 		/*const string inputfileA = Input("--input_A", "input filename of A", "./A.txt");
