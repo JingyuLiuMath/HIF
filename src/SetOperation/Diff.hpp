dfd4119389ec95c4a75ee94ed879f2c713c05dc8
Jingyu <381258337@qq.com>
2022-07-24 00:52:36 +0800

    update diff

--- src/SetOperation/Diff.hpp
+++ src/SetOperation/Diff.hpp
@@ -8,65 +8,9 @@ void Diff_Sort(const vector<Scalar>& vec1, const vector<Scalar>& vec2, vector<Sc
 {
 	DEBUG_HIF(CallStackEntry cse("Diff_Sort"))
 
-	//TIMER_HIF(TimerStart(TIMER_SETOP))
-	//int i = 0;
-	//int j = 0;
-	//int startj = -1;
-	//int endj = -1;
-	//int tmpindex = -1;
-	//while (i < vec1.size() && j < vec2.size())
-	//{
-	//	if (vec1[i] < vec2[j])
-	//	{
-	//		vec.push_back(vec1[i]);
-	//		i++;
-	//	}
-	//	else if (vec1[i] > vec2[j])
-	//	{
-	//		if (vec1[i] > vec2.back())
-	//		{
-	//			break;
-	//		}
-	//		// Find j where j is the min index such that vec2[j] >= vec1[i].
-	//		startj = j + 1;
-	//		endj = vec2.size() - 1;
-	//		while (startj <= endj)
-	//		{
-	//			tmpindex = startj + (endj - startj) / 2;
-	//			if (vec2[tmpindex] < vec1[i])
-	//			{
-	//				startj = tmpindex + 1;
-	//			}
-	//			else if (vec2[tmpindex] > vec1[i])
-	//			{
-	//				endj = tmpindex - 1;
-	//			}
-	//			else
-	//			{
-	//				startj = tmpindex;
-	//				break;
-	//			}
-	//		}
-	//		j = startj;
-	//	}
-	//	else
-	//	{
-	//		i++;
-	//		j++;
-	//	}
-	//}
-	//while (i < vec1.size())
-	//{
-	//	vec.push_back(vec1[i]);
-	//	i++;
-	//}
-	//TIMER_HIF(TimerStop(TIMER_SETOP))
-	
 	TIMER_HIF(TimerStart(TIMER_SETOP))
-	vec.resize(vec1.size());
 	int i = 0;
 	int j = 0;
-	int actualsize_vec = 0;
 	int startj = -1;
 	int endj = -1;
 	int tmpindex = -1;
@@ -74,8 +18,7 @@ void Diff_Sort(const vector<Scalar>& vec1, const vector<Scalar>& vec2, vector<Sc
 	{
 		if (vec1[i] < vec2[j])
 		{
-			vec[actualsize_vec] = vec1[i];
-			actualsize_vec++;
+			vec.push_back(vec1[i]);
 			i++;
 		}
 		else if (vec1[i] > vec2[j])
@@ -89,7 +32,7 @@ void Diff_Sort(const vector<Scalar>& vec1, const vector<Scalar>& vec2, vector<Sc
 			endj = vec2.size() - 1;
 			while (startj <= endj)
 			{
-				tmpindex = (startj + endj) / 2;
+				tmpindex = startj + (endj - startj) / 2;
 				if (vec2[tmpindex] < vec1[i])
 				{
 					startj = tmpindex + 1;
@@ -114,11 +57,9 @@ void Diff_Sort(const vector<Scalar>& vec1, const vector<Scalar>& vec2, vector<Sc
 	}
 	while (i < vec1.size())
 	{
-		vec[actualsize_vec] = vec1[i];
-		actualsize_vec++;
+		vec.push_back(vec1[i]);
 		i++;
 	}
-	vec.erase(vec.begin() + actualsize_vec, vec.end());
 	TIMER_HIF(TimerStop(TIMER_SETOP))
 }
 
