bb6b3b69b16777d138f5cf147207892e4d8d3b1c
Jingyu <381258337@qq.com>
2022-07-24 13:49:54 +0800

    update setnbnode

--- src/HIFGraph/SetNeighborNode.hpp
+++ src/HIFGraph/SetNeighborNode.hpp
@@ -14,55 +14,6 @@ void HIFGraph<Scalar>::SetNeighborNode()
 		return;
 	}
 
-	//for (int iter = 0; iter < 2; iter++)
-	//{
-	//	HIFGraph* childnode = children_[iter];
-	//	(childnode->nbnode_).push_back(children_[1 - iter]);
-	//	(childnode->nbnodeseqnum_).push_back(children_[1 - iter]->seqnum_);
-	//	(childnode->nbnodelevel_).push_back(children_[1 - iter]->level_);
-	//	for (int i = 0; i < nbnode_.size(); i++)
-	//	{
-	//		HIFGraph* nbnodei = nbnode_[i];
-	//		if (nbnodei->endflag_ == 1)
-	//		{
-	//			// Now nbnodei doesn't have a child, we should treat it as a nbnode.
-	//			if (Intersect_Sort(childnode->nb_, nbnodei->vtx_))
-	//			{
-	//				// We have to avoid add one's ancestor as its nbnode.
-	//				int dlevel = childnode->level_ - nbnodei->level_;
-	//				int myseqnum = childnode->seqnum_;
-	//				for (int k = 0; k < dlevel; k++)
-	//				{
-	//					myseqnum = myseqnum / 2;
-	//				}
-	//				if (myseqnum == nbnodei->seqnum_)
-	//				{
-	//					continue;
-	//				}
-	//				(childnode->nbnode_).push_back(nbnodei);
-	//				(childnode->nbnodeseqnum_).push_back(nbnodei->seqnum_);
-	//				(childnode->nbnodelevel_).push_back(nbnodei->level_);
-	//				/*(nbnodei->nbnode_).push_back(childnode);
-	//				(nbnodei->nbnodeseqnum_).push_back(childnode->seqnum_);
-	//				(nbnodei->nbnodelevel_).push_back(childnode->level_);*/
-	//			}
-	//		}
-	//		else
-	//		{
-	//			for (int it = 0; it < 2; it++)
-	//			{
-	//				HIFGraph* childnbnodei = nbnodei->children_[it];
-	//				if (Intersect_Sort(childnode->nb_, childnbnodei->vtx_))
-	//				{
-	//					(childnode->nbnode_).push_back(childnbnodei);
-	//					(childnode->nbnodeseqnum_).push_back(childnbnodei->seqnum_);
-	//					(childnode->nbnodelevel_).push_back(childnbnodei->level_);
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
-
 	for (int iter = 0; iter < 2; iter++)
 	{
 		HIFGraph& childnode = childrennode_.Child(iter);
