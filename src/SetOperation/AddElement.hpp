c0aa04170b869bcb8ed4009bf54947cd5b7368de
Jingyu <381258337@qq.com>
2022-07-25 18:29:36 +0800

    update addvec

--- src/SetOperation/AddElement.hpp
+++ src/SetOperation/AddElement.hpp
@@ -14,7 +14,33 @@ void AddVec_Sort(vector<Scalar>& vec1, const vector<Scalar>& vec2)
 		TIMER_HIF(TimerStop(TIMER_SETOP))
 		return;
 	}
+	if (vec1.size() == 0)
+	{
+		vec1.assign(vec2.begin(), vec2.end());
+		TIMER_HIF(TimerStop(TIMER_SETOP))
+		return;
+	}
+	if (vec1.back() < vec2.front())
+	{
+		for (int k = 0; k < vec2.size(); k++)
+		{
+			vec1.push_back(vec2[k]);
+		}
+		TIMER_HIF(TimerStop(TIMER_SETOP))
+		return;
+	}
 	vector<Scalar> vec;
+	if (vec1.front() > vec2.back())
+	{
+		vec.assign(vec2.begin(), vec2.end());
+		for (int k = 0; k < vec1.size(); k++)
+		{
+			vec.push_back(vec1[k]);
+		}
+		vec1.assign(vec.begin(), vec.end());
+		TIMER_HIF(TimerStop(TIMER_SETOP))
+		return;
+	}
 	int i = 0;
 	int j = 0;
 	while (i < vec1.size() && j < vec2.size())
