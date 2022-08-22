7dc6beadbaf40e25a998b07de1e0e2dfebadcfb8
Jingyu <381258337@qq.com>
2022-08-22 17:13:13 +0800

    update HIFsolve

--- test/HIFSolve/HIFSolve.cpp
+++ test/HIFSolve/HIFSolve.cpp
@@ -11,20 +11,20 @@
 
 using namespace HIF;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-	// El::Initialize(argc, argv);
+	El::Initialize(argc, argv);
 
 	try
 	{
-		/*const string inputfileA = Input("--input_A", "input filename of A", "./A.txt");
+		const string inputfileA = Input("--input_A", "input filename of A", "./A.txt");
 		const string inputfileb = Input("--input_b", "input filename of b", "./b.txt");
 		const int minvtx = Input("--minvtx", "minvtx", 64);
 		const bool button = Input("--HIFbutton", "true for HIF, false for MF", true);
 		const double tol = Input("--tol", "tolerance", 1e-3);
-		const bool logApp = Input("--logApp", "Log File appending", false);*/
+		const bool logApp = Input("--logApp", "Log File appending", false);
 		
-		if (argc != 7)
+		/*if (argc != 7)
 		{
 			printf("HIFsolve, Usage: ./HIFsolve input_A input_b minvtx button tol logApp\n");
 			exit(-1);
@@ -57,13 +57,13 @@ int main(int argc, char** argv)
 		std::cout << "minvtx: " << minvtx << std::endl;
 		std::cout << "button: " << button << std::endl;
 		std::cout << "tol: " << tol << std::endl;
-		std::cout << "logApp: " << logApp << std::endl;
+		std::cout << "logApp: " << logApp << std::endl;*/
 
-		// El::ProcessInput();
+		El::ProcessInput();
 		LogAppend(logApp);
 		SetTol(tol);
 		SetButton(button);
-		// El::PrintInputReport();
+		El::PrintInputReport();
 
 		El::SetBlocksize(128);
 		El::SetLocalTrrkBlocksize<double>(8);
@@ -175,6 +175,6 @@ int main(int argc, char** argv)
 		DEBUG_HIF(DumpCallStack())
 	}
 	CloseLog();
-	// El::Finalize();
+	El::Finalize();
 	return 0;
 }
