7dd5ae913f311e421ef4796a9d2f53acbb17864c
Jingyu <381258337@qq.com>
2022-09-02 10:37:56 +0800

    update testHIFSolve

--- test/HIFSolve/testHIFSolve.cpp
+++ test/HIFSolve/testHIFSolve.cpp
@@ -22,6 +22,7 @@ int main(int argc, char* argv[])
 	{
 		const string inputfileA = Input("--input_A", "input filename of A", "./A.txt");
 		const string inputfileb = Input("--input_b", "input filename of b", "./b.txt");
+		const string inputfilex = Input("--input_x", "input filename of x", "./x.txt");
 		const int minvtx = Input("--minvtx", "minvtx", 64);
 		const bool button = Input("--HIFbutton", "true for HIF, false for MF", true);
 		const double tol = Input("--tol", "tolerance", 1e-3);
@@ -78,9 +79,25 @@ int main(int argc, char* argv[])
 		}
 		finb.close();
 
+		string filex = inputfilex;
+		std::ifstream finx;
+		finx.open(filex, std::ios::in);
+		if (!finx)
+		{
+			std::cerr << "cannot open the file" << std::endl;;
+		}
+		Matrix<double> x(n, 1);
+		int k = 0;
+		while (finx >> value)
+		{
+			x.Set(k, 0, value);
+			k++;
+		}
+		finx.close();
+
 		readTimer.Stop();
 		MasterCout("Reading input ends in ", readTimer.Total(), " sec.");
-		
+
 		// Initialization.
 		El::Timer initTimer("InitTimer");
 		initTimer.Start();
@@ -113,11 +130,13 @@ int main(int argc, char* argv[])
 
 		// We already know the solution is 1.
 		double relnorm2 = 0.0;
+		double xnorm2 = 0.0;
 		for (int k = 0; k < n; k++)
 		{
-			relnorm2 += std::pow(b.Get(k, 0) - 1, 2);
+			relnorm2 += std::pow(b.Get(k, 0) - x.Get(k, 0), 2);
+			xnorm2 += std::pow(x.Get(k, 0), 2);
 		}
-		relnorm2 = relnorm2 / n;
+		relnorm2 = relnorm2 / xnorm2;
 		relnorm2 = std::sqrt(relnorm2);
 
 
@@ -126,23 +145,23 @@ int main(int argc, char* argv[])
 			Log(
 				"        InitT        FactT        SolT        RelErr2"
 			);
-			Log(
-				setw(13), scientific, setprecision(2), initTimer.Total(),
-				setw(13), scientific, setprecision(2), factTimer.Total(),
-				setw(12), scientific, setprecision(2), solTimer.Total(),
-				setw(15), scientific, setprecision(2), relnorm2
-				);
-			Log(
-				"==================================================",
-				"==================================================",
-				"=================================================="
-			);
+		Log(
+			setw(13), scientific, setprecision(2), initTimer.Total(),
+			setw(13), scientific, setprecision(2), factTimer.Total(),
+			setw(12), scientific, setprecision(2), solTimer.Total(),
+			setw(15), scientific, setprecision(2), relnorm2
+		);
+		Log(
+			"==================================================",
+			"==================================================",
+			"=================================================="
+		);
 		)
 	}
 	catch (std::exception& e)
 	{
 		EL_DEBUG_ONLY(El::DumpCallStack())
-		DEBUG_HIF(DumpCallStack())
+			DEBUG_HIF(DumpCallStack())
 	}
 	CloseLog();
 	El::Finalize();
